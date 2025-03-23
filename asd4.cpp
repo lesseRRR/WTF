ec6fec097249335df3cc28dca9433bda03ab4c7e
lesseRRR <dikkir6@gmail.com>
2025-03-23 22:53:32 +0300

    Add files via upload

---
+++ asd4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+
+
+
+int main(){
+    _setmode(_fileno(stdout),_O_U16TEXT);
+    setlocale(LC_ALL,"Russian");
+    int v1[10], v2[10];
+    for(int i = 0; i < 10; i++) v1[i] = v2[i];
+    std::wcout<<v1[5]<<L"----"<<v2[5]<<std::endl;
+    return 0;
+}
